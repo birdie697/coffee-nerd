5b7d6f53395a08831a9660cc25d11e7fe3468edc
Marc Marino <marc.a.marino@gmail.com>
2019-02-06 16:01:22 -0500

    set up auto coffee seeds for new user sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-has_many :preparations
-has_many :usercoffees
-has_many :coffees, through: :usercoffees
+
+  after_create :load_generic_coffees, on: :create
+
+  has_many :preparations
+  has_many :usercoffees
+  has_many :coffees, through: :usercoffees
+
+  private
+    def load_generic_coffees
+      newest_user = User.order(created_at: :desc).limit(1).pluck(:id)[0]
+      Usercoffee.create(user_id: newest_user, coffee_id: Coffee.where(name: "Generic Light Roast").pluck(:id)[0])
+      Usercoffee.create(user_id: newest_user, coffee_id: Coffee.where(name: "Generic Medium Roast").pluck(:id)[0])
+      Usercoffee.create(user_id: newest_user, coffee_id: Coffee.where(name: "Generic Dark Roast").pluck(:id)[0])
+    end
 end
